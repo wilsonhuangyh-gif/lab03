fa002f8d97eb74d48de12b3ce14d522a45fe8072
Willyfrys <soupwilly@gmail.com>
2026-09-22 15:21:19 -0400

    Update lab03.py

--- lab03.py
+++ lab03.py
@@ -10,18 +10,30 @@ def pig_latin(word):
     # TODO (Part 1): return the Pig Latin form of a single lowercase word.
     #   If it starts with a vowel (a, e, i, o, u): add "way" to the end.
     #   Otherwise: move the first letter to the end and add "ay".
+    vowels = ['a','e','i','o','u']
+    if word[0] in vowels:
+        word = word +'way'
+    else:
+        return word[1:] + word[0] + 'ay'    
+    return word
+
     pass
 
 
 def word_lengths(sentence):
     # TODO (Part 2): return a list with the length of each word in `sentence`
     #   (words are separated by spaces).
+    for i in sentence.split():
+        a = (len(i))
+        print(a)
+        
     pass
 
 
 def reverse_words(sentence):
     # TODO (Part 3): return `sentence` with the order of its words reversed.
     #   e.g. "hello world" -> "world hello"
+
     pass
 
 
@@ -34,8 +46,8 @@ def letter_counts(text):
 
 def main():
     # Optional scratch space - use this to try your functions with sample values.
-    # print(pig_latin("banana"))                    # ananabay
-    # print(word_lengths("the quick brown fox"))    # [3, 5, 5, 3]
+    #print(pig_latin("banana"))                    # ananabay
+    print(word_lengths("the quick brown fox"))    # [3, 5, 5, 3]
     # print(reverse_words("the quick brown fox"))   # fox brown quick the
     # print(letter_counts("hello"))                 # {'h': 1, 'e': 1, 'l': 2, 'o': 1}
     pass
